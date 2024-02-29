5d1ff495449c2edd50be4f1292a46d9f4e64f691
Mohd Zaid <101700541+MohdZaid5@users.noreply.github.com>
2024-02-29 14:50:34 +0530

    Create IUniverse.cs

---
+++ Core/Interfaces/IUniverse.cs
@@ -0,0 +1,13 @@
+﻿namespace Core
+{
+    /// <summary>
+    /// Represents an <c>Universe</c> where the next <c>Generation</c> of cells can be computed.
+    /// </summary>
+    public interface IUniverse
+    {
+        /// <summary>
+        /// Computes the next <c>Generation</c> of cells in the universe.
+        /// </summary>
+        void NextGeneration();
+    }
+}
