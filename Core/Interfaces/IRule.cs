aea88f3583b196f58fb5d9fad91b262538e9c3d6
Mohd Zaid <101700541+MohdZaid5@users.noreply.github.com>
2024-02-29 16:41:24 +0530

    minor tweaks

--- Core/Interfaces/IRule.cs
+++ Core/Interfaces/IRule.cs
@@ -1,4 +1,5 @@
-﻿namespace Core.Interfaces
+﻿
+namespace Core.Interfaces
 {
     /// <summary>
     /// Represents a <c>Rule</c> which determines the fate of a <c>Cell</c> in the next <c>Generation</c>.
@@ -7,11 +8,15 @@
     {
         /// <summary>
         /// Applies the rule to determine the state of the specified <paramref name="cell"/> in the next generation
-        /// based on the number of alive neighbors <paramref name="nAliveNeighbors"/>.
+        /// based on the states of its neighboring cells.
         /// </summary>
         /// <param name="cell">The <c>Cell</c> for which the rule is being applied.</param>
-        /// <param name="nAliveNeighbors">The number of alive neighbors of the <paramref name="cell"/>.</param>
+        /// <param name="neighbors">Array representing the states of neighboring cells in the following order:
+        ///     [0][1][2]
+        ///     [3][C][4]
+        ///     [5][6][7]
+        ///     where C represents the current cell.</param>
         /// <returns>True if the cell should be alive in the next generation; otherwise, false.</returns>
-        bool Apply(ICell cell, int nAliveNeighbors);
+        bool Apply(ICell cell, ICell[] neighbors);
     }
 }
