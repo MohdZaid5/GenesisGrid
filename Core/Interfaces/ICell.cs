4ff9974718369234e7a838a7f0569cb789517a97
Mohd Zaid <101700541+MohdZaid5@users.noreply.github.com>
2024-02-29 14:50:26 +0530

    Create ICell.cs

---
+++ Core/Interfaces/ICell.cs
@@ -0,0 +1,19 @@
+﻿namespace Core
+{
+    /// <summary>
+    /// Represents a <c>Cell</c> in <b>Conway's Game Of Life</b>.
+    /// </summary>
+    /// <remarks>
+    /// It holds the <c>state</c> of a <c>Cell</c>.
+    /// </remarks>
+    public interface ICell
+    {
+        /// <summary>
+        /// Gets or sets the current <c>state</c> of the <c>Cell</c>.
+        /// </summary>
+        /// <remarks>
+        /// It represents the state of the cell, whether it is alive or dead.
+        /// </remarks>
+        bool IsAlive { get; set; }
+    }
+}
