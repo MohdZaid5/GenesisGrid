8c3b587439ca7448e21bb8b673dd51d9ffd249d0
Mohd Zaid <101700541+MohdZaid5@users.noreply.github.com>
2024-03-02 08:18:02 +0530

    displaced from services

---
+++ Core/Elementary/Cell.cs
@@ -0,0 +1,15 @@
+﻿using Core.Interfaces;
+
+namespace Core.Elementary
+{
+    public class Cell(bool isAlive) : ICell
+    {
+        private bool _isAlive = isAlive;
+
+        public bool IsAlive
+        {
+            get { return _isAlive; }
+            set { _isAlive = value; }
+        }
+    }
+}
